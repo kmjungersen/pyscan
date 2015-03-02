73435aefaca243aa0d737bee48fb6a7c6297075e
kmjungersen <kmjungersen@gmail.com>
2015-03-01 22:17:43 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+
+"""
+
+# imports
+import json
+import urllib
+
+from local import *
+
+
+class Barcode():
+
+    def __init__(self, barcode_number):
+        """
+
+        :return:
+        """
+
+        bar = barcode_number
+
+        base_url =
