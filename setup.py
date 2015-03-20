9883c07f26f4ded215ab694461429e712b4e442c
kmjungersen <kmjungersen@gmail.com>
2015-03-20 13:07:33 -0400

    added an install script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    description='Pyscan!',
+    version='0.1',
+    name='pyscan',
+    packages=[
+        'pyscan',
+    ]
+)
